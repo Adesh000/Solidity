55977bdb45c18b5b7397e207c3cc556c9b237cd2
Adesh Nayak <adeshnayak6@gmail.com>
2023-02-27 16:01:24 +0530

    assignment added

---
+++ BigginerAssessment/SimpleStorage.sol
@@ -0,0 +1,20 @@
+//SPDX-license-Identifier: UNLICENSED
+
+//Tell the Solidity compiler what version to use
+pragma solidity ^0.8.8;
+
+//Declares a new contract
+contract SimpleStorage {
+    //Storage. Persists in between transactions
+    uint x;
+
+    //Allows the unsigned integer stored to be changed
+    function set(uint newValue) public {
+        x = newValue;
+    }
+    
+    //Returns the currently stored unsigned integer
+    function get() public view returns (uint) {
+        return x;
+    }
+}
